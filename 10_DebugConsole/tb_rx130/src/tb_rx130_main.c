6218d3ef7feb24728e00e5c2113ce36dabf85bed
NoMaY (a user of Japan.RenesasRulz.com) <NoMaY-jp@outlook.com>
2020-10-28 10:01:23 +0900

    Add a Debug Console example

---
+++ 10_DebugConsole/tb_rx130/src/tb_rx130_main.c
@@ -0,0 +1,27 @@
+/***********************************************************************
+*
+*  FILE        : tb_rx130_main.cpp
+*  DATE        : 2019-02-26
+*  DESCRIPTION : Main Program
+*
+*  NOTE:THIS IS A TYPICAL EXAMPLE.
+*
+***********************************************************************/
+#include "r_smc_entry.h"
+#include "tbrx130def.h"
+
+int main(void)
+{
+    LED0 = LED_ON;
+
+    for (;;)
+    {
+        printf( "Hello World\r\n" );
+
+        R_BSP_SoftwareDelay( 1000, BSP_DELAY_MILLISECS );
+        LED0 = ~LED0;
+
+        R_BSP_SoftwareDelay( 1000, BSP_DELAY_MILLISECS );
+        LED0 = ~LED0;
+    }
+}
---
+++ 10_DebugConsole/tb_rx65n/src/tb_rx65n_main.c
@@ -0,0 +1,27 @@
+/***********************************************************************
+*
+*  FILE        : tb_rx65n_main.cpp
+*  DATE        : 2019-02-26
+*  DESCRIPTION : Main Program
+*
+*  NOTE:THIS IS A TYPICAL EXAMPLE.
+*
+***********************************************************************/
+#include "r_smc_entry.h"
+#include "tbrx65ndef.h"
+
+int main(void)
+{
+    LED0 = LED_ON;
+
+    for (;;)
+    {
+        printf( "Hello World\r\n" );
+
+        R_BSP_SoftwareDelay( 1000, BSP_DELAY_MILLISECS );
+        LED0 = ~LED0;
+
+        R_BSP_SoftwareDelay( 1000, BSP_DELAY_MILLISECS );
+        LED0 = ~LED0;
+    }
+}
